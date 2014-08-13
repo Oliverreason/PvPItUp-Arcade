63c8cdb394367ebf1b242db63ba229a46bccfb92
Oliverreason <oliverreason@hotmail.co.uk>
2014-08-13 14:21:43 +0100

    Update VoteCommand.java

--- src/com/pvpitup/arcade/commands/VoteCommand.java
+++ src/com/pvpitup/arcade/commands/VoteCommand.java
@@ -9,6 +9,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import com.pvpitup.arcade.tempData;
+import com.pvpitup.arcade.Arcade;
 
 public class VoteCommand implements CommandExecutor {
 	
