802959bfed654bd1fbcd43bc3d908c944f2b6eb9
Oliverreason <oliverreason@hotmail.co.uk>
2014-08-13 14:36:09 +0100

    Update onQuit.java

--- src/com/pvpitup/arcade/events/onQuit.java
+++ src/com/pvpitup/arcade/events/onQuit.java
@@ -1,7 +1,5 @@
 package com.pvpitup.arcade.events;
 
-import org.bukkit.ChatColor;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
@@ -10,7 +8,6 @@ public class onQuit implements Listener {
 	
 	@EventHandler
 	public void onPlayerQuit(PlayerQuitEvent e) {
-		Player p = e.getPlayer();
 		e.setQuitMessage(null);
 	}
 }
