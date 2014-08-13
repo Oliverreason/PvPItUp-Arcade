7fa7a8511ad664996c61cc9500883a48173212ca
Oliverreason <oliverreason@hotmail.co.uk>
2014-08-13 21:14:38 +0100

    Create Spectators.java

---
+++ src/com/pvpitup/arcade/teams/teamstypes/Spectators.java
@@ -0,0 +1,25 @@
+package com.pvpitup.arcade.teams.teamstypes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.ChatColor;
+
+import com.pvpitup.arcade.teams.Team;
+
+public class Spectators implements Team {
+	
+	private List<String> players = new ArrayList<String>();
+
+	public String getName() {
+		return "Spectators";
+	}
+
+	public List<String> getPlayers() {
+		return players;
+	}
+
+	public ChatColor getColor() {
+		return ChatColor.LIGHT_PURPLE;
+	}
+}
